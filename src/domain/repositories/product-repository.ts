9b61f9dd7188a862929288537c09392c9c9e1541
Gabriel-Henrique00 <gabriel500henrique@gmail.com>
2025-08-09 15:55:43 -0300

    feat: add new product in accessories category

--- src/domain/repositories/product-repository.ts
+++ src/domain/repositories/product-repository.ts
@@ -4,7 +4,7 @@ export class ProductRepository {
     private products: Product[] = [
         { id: 'sku-001', name: 'Product A', unitPrice: 79.9, category: 'electronics' },
         { id: 'sku-002', name: 'Product B', unitPrice: 39.9, category: 'acessorios' },
-        { id: 'sku-003', name: 'Product C', unitPrice: 19.9, category: 'books' },
+        { id: 'sku-003', name: 'Product C', unitPrice: 19.9, category: 'acessorios' },
         { id: 'sku-004', name: 'Product D', unitPrice: 1500.0, category: 'electronics' },
         { id: 'sku-005', name: 'Product E', unitPrice: 5.0, category: 'acessorios' }
     ];
